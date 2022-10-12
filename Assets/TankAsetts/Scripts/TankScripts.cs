61db4853ac7e317fe906d3f3e5c55c686e506457
uchidaCreator <kouei28poiou@icloud.com>
2022-10-12 12:13:32 +0900

    Chenaged 2022/10/12

---
+++ Assets/TankAsetts/Scripts/TankScripts.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankScripts : MonoBehaviour
+{
+    private float ForWedSpeed = 2;
+    [SerializeField] private float RotateSpeed = 0.1f; 
+     public GameObject Upper = null;
+     public GameObject Canon = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            this.transform.Translate(0, 0, ForWedSpeed*Time.deltaTime);
+        }
+       if(Input.GetKey(KeyCode.A))
+        {
+            this.transform.Rotate(0, RotateSpeed, 0);
+        }
+       if(Input.GetKey(KeyCode.D))
+        {
+            this.transform.Rotate(0, -RotateSpeed, 0);  
+        }
+       if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            Upper.transform.Rotate(0, -RotateSpeed, 0);
+        }
+       if(Input.GetKey(KeyCode.RightArrow))
+        {
+            Upper.transform.Rotate(0, RotateSpeed, 0);
+        }
+    }
+}
